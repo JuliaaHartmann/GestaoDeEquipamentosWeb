ee138cc4752ac3e980574d30238a49727771a330
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-05-18 20:13:39 -0300

    Adicionada Listagem de Equipamentos

---
+++ GestaoDeEquipamentosWeb.ConsoleApp/Models/EquipamentoViewModels.cs
@@ -0,0 +1,10 @@
+namespace GestaoDeEquipamentosWeb.ConsoleApp.Models;
+
+
+public record ListarEquipamentosViewModel(
+    string Id,
+    string Nome,
+    decimal PrecoAquisicao,
+    DateTime DataFabricacao,
+    string Fabricante
+);
